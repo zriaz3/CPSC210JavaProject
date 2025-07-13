351862cddf3d0405831dfbbfa805c3ee537a47d9
Zain Riaz <zriaz3@students.cs.ubc.ca>
2025-07-13 13:11:49 -0700

    Added specification for FindDogApp

---
+++ src/main/ui/FindDogApp.java
@@ -0,0 +1,22 @@
+package ui;
+
+import java.util.Scanner;
+
+// Dog application 
+public class FindDogApp {
+
+    private Scanner scanner;
+
+    // EFFECTS: runs the dog application
+    public FindDogApp() {
+
+    }
+
+    // EFFECTS: choose which version to run based on if person has lost or found a dog
+    public void runDogApp() {
+
+    }
+    public static void main(String[] args) {
+        new FindDogApp();
+    }
+}
