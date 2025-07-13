06b5a54e8c6616952323f9b171690b162d24a847
Zain Riaz <zriaz3@students.cs.ubc.ca>
2025-07-12 22:33:01 -0700

    Added tests for Dog class

---
+++ src/test/model/DogTest.java
@@ -0,0 +1,33 @@
+package model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DogTest {
+
+    private Dog testDog;
+    
+    @BeforeEach
+    void runBefore() {
+        testDog = new Dog("Cutie", 7, "Border Collie", "White", "Small", "Lean", "images/Cutie.PNG");
+    }
+
+    @Test
+    public void testConstructor() {
+        assertEquals("Cutie", testDog.getName());
+        assertEquals(7, testDog.getAge());
+        assertEquals("Border Collie", testDog.getBreed());
+        assertEquals("White", testDog.getFurColor());
+        assertEquals("Small", testDog.getSize());
+        assertEquals("Lean", testDog.getBuild());
+        assertEquals("images/Cutie.PNG", testDog.getPicture());
+    }
+
+    @Test
+    public void testToString() {
+        assertEquals(testDog.toString(), 
+        "/nName: Cutie/nAge: 7/nBreed: Border Collie/nFur Color: White/nSize: Small/nBuild: Lean/nPicture: images/Cutie.PNG");
+    }
+}
