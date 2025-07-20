9bc7e57e77aec7db868aae20c459d90d407cf4b5
Zain Riaz <zriaz3@students.cs.ubc.ca>
2025-07-20 02:42:17 -0700

    Implemented json reader

--- src/main/persistence/JsonReader.java
+++ src/main/persistence/JsonReader.java
@@ -10,7 +10,6 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.stream.Stream;
 
 import org.json.*;
@@ -22,33 +21,103 @@ public class JsonReader {
 
     // EFFECTS: constructs a reader to read from source file
     public JsonReader(String source) {
-     this.source = source;
+        this.source = source;
     }
 
     // EFFECTS reads app data from file and returns it;
     // throws IOException if an error occurs reading data from file
     public Object[] read() throws IOException {
-        return null;
+        String jsonData = readFile(source);
+        JSONObject jsonObject = new JSONObject(jsonData);
+        return parseFindDogApp(jsonObject);
     }
 
+    // EFFECTS: reads source file as string and returns it
+    // throws IOException if an error occurs reading data from file
     private String readFile(String source) throws IOException {
-        return null;
+        StringBuilder contentBuilder = new StringBuilder();
+
+        try (Stream<String> stream = Files.lines(Paths.get(source), StandardCharsets.UTF_8)) {
+            stream.forEach(s -> contentBuilder.append(s));
+        }
+
+        return contentBuilder.toString();
     }
 
+    // EFFECTS: parses currentDog, listPersonLost and listPersonFound from JSON
+    // object and returns it
     private Object[] parseFindDogApp(JSONObject jsonObject) {
-        return null;
+        Dog currentDog = null;
+        if (!jsonObject.isNull("currentDog")) {
+            currentDog = parseDog(jsonObject.getJSONObject("currentDog"));
+        }
+
+        ListPersonLost listPersonLost = parseListPersonLost(jsonObject);
+        ListPersonFound listPersonFound = parseListPersonFound(jsonObject);
+
+        return new Object[] { currentDog, listPersonLost, listPersonFound };
     }
 
+    // MODIFIES: listPersonLost
+    // EFFECTS: parses PersonLost from JSON object and adds them to listPersonLost
+    private ListPersonLost parseListPersonLost(JSONObject jsonObject) {
+        ListPersonLost listPersonLost = new ListPersonLost();
+        JSONArray jsonArray = jsonObject.getJSONArray("listPersonLost");
+
+        for (Object json : jsonArray) {
+            JSONObject lostPerson = (JSONObject) json;
+            PersonLost person = parsePersonLost(lostPerson);
+            listPersonLost.addPerson(person);
+        }
+        return listPersonLost;
+    }
+
+    // MODIFIES: listPersonFound
+    // EFFECTS: parses PersonFound from JSON object and adds them to listPersonFound
+    private ListPersonFound parseListPersonFound(JSONObject jsonObject) {
+        ListPersonFound listPersonFound = new ListPersonFound();
+        JSONArray jsonArray = jsonObject.getJSONArray("listPersonFound");
+
+        for (Object json : jsonArray) {
+            JSONObject foundPerson = (JSONObject) json;
+            PersonFound person = parsePersonFound(foundPerson);
+            listPersonFound.addPerson(person);
+        }
+        return listPersonFound;
+    }
+
+    // EFFECTS: parses Dog from JSON object and returns it
     private Dog parseDog(JSONObject jsonObject) {
-        return new Dog(source, 0, source, source, source, source, source);
+        String name = jsonObject.getString("name");
+        int age = jsonObject.getInt("age");
+        String breed = jsonObject.getString("breed");
+        String furColor = jsonObject.getString("furColor");
+        String size = jsonObject.getString("size");
+        String build = jsonObject.getString("build");
+        String picture = jsonObject.getString("picture");
+
+        return new Dog(name, age, breed, furColor, size, build, picture);
     }
 
-    private ListPersonLost parseListPersonLost(JSONArray jsonArray) {
-        return new ListPersonLost();
+    // EFFECTS: parses PersonLost from JSON object and returns it
+    private PersonLost parsePersonLost(JSONObject jsonObject) {
+        String name = jsonObject.getString("name");
+        String phoneNumber = jsonObject.getString("phoneNumber");
+        String location = jsonObject.getString("location");
+        String timeLost = jsonObject.getString("timeLost");
+        Dog dog = parseDog(jsonObject.getJSONObject("dog"));
+
+        return new PersonLost(name, phoneNumber, location, timeLost, dog);
     }
 
-    private ListPersonFound parseListPersonFound(JSONArray jsonArray) {
-        return new ListPersonFound();
+    // EFFECTS: parses PersonFound from JSON object and returns it
+    private PersonFound parsePersonFound(JSONObject jsonObject) {
+        String name = jsonObject.getString("name");
+        String phoneNumber = jsonObject.getString("phoneNumber");
+        String location = jsonObject.getString("location");
+        String timeLost = jsonObject.getString("timeFound");
+        Dog dog = parseDog(jsonObject.getJSONObject("dog"));
 
-}
+        return new PersonFound(name, phoneNumber, location, timeLost, dog);
+    }
 }
--- src/test/persistence/JsonTest.java
+++ src/test/persistence/JsonTest.java
@@ -6,9 +6,12 @@ import model.PersonFound;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+// Represents helper functions used in json tests
+// Modeled after sample application provided
 public class JsonTest {
 
-    protected void checkDog(String name, int age, String breed, String furColor, String size, String build, String picture, Dog d) {
+    protected void checkDog(String name, int age, String breed, String furColor, 
+                            String size, String build, String picture, Dog d) {
         assertEquals(name, d.getName());
         assertEquals(age, d.getAge());
         assertEquals(breed, d.getBreed());
@@ -18,19 +21,23 @@ public class JsonTest {
         assertEquals(picture, d.getPicture());
     }
 
-    protected void checkPersonLost(String name, String phoneNumber, String location, String timeLost, Dog dog, PersonLost p) {
+    protected void checkPersonLost(String name, String phoneNumber, String location, 
+                                   String timeLost, Dog dog, PersonLost p) {
         assertEquals(name, p.getName());
         assertEquals(phoneNumber, p.getPhoneNumber());
         assertEquals(location, p.getLocation());
         assertEquals(timeLost, p.getTimeLost());
-        assertEquals(dog, p.getDog());
+        checkDog(dog.getName(), dog.getAge(), dog.getBreed(), dog.getFurColor(), 
+                 dog.getSize(), dog.getBuild(), dog.getPicture(), p.getDog());
     }
 
-    protected void checkPersonFound(String name, String phoneNumber, String location, String timeFound, Dog dog, PersonFound p) {
+    protected void checkPersonFound(String name, String phoneNumber, String location, 
+                                    String timeFound, Dog dog, PersonFound p) {
         assertEquals(name, p.getName());
         assertEquals(phoneNumber, p.getPhoneNumber());
         assertEquals(location, p.getLocation());
         assertEquals(timeFound, p.getTimeFound());
-        assertEquals(dog, p.getDog());
+        checkDog(dog.getName(), dog.getAge(), dog.getBreed(), dog.getFurColor(), 
+                 dog.getSize(), dog.getBuild(), dog.getPicture(), p.getDog());
     }
 }
