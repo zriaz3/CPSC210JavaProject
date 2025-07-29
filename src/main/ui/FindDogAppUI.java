d778eb3b6cb6b776ba6ebfe08239a6bbd77e1f69
Zain Riaz <zriaz3@students.cs.ubc.ca>
2025-07-29 01:02:42 -0700

    added basic specification for GUI

---
+++ src/main/ui/FindDogAppUI.java
@@ -0,0 +1,25 @@
+package ui;
+
+import javax.swing.*;
+
+public class FindDogAppUI extends JFrame {
+
+    public static final int WIDTH = 750;
+    public static final int HEIGHT = 750;
+
+    public FindDogAppUI() {
+        addButtonPanel();
+        addMenu();
+    }
+
+    private void addButtonPanel() {
+
+    }
+
+    private void addMenu() {
+    }
+
+    public static void main(String[] args) {
+        new FindDogAppUI();
+    }
+}
