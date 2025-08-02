45f83c8819f6bceda1bc775e2581b0395f4db277
Zain Riaz <zriaz3@students.cs.ubc.ca>
2025-08-02 16:00:14 -0700

    implemented FindDogAppUI

--- src/main/ui/FindDogAppUI.java
+++ src/main/ui/FindDogAppUI.java
@@ -1,10 +1,13 @@
 package ui;
 
-import java.util.Scanner;
-
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import model.CurrentDog;
+import model.Dog;
 import model.ListPersonFound;
 import model.ListPersonLost;
 import persistence.JsonReader;
@@ -30,41 +33,121 @@ public class FindDogAppUI extends JFrame {
     private JsonReader jsonReader;
 
     // modelled after drawing editor
+    // Github link: https://github.students.cs.ubc.ca/CPSC210/SimpleDrawingPlayer-Complete
     // EFFECTS: initializes and runs the Find Dog app
     public FindDogAppUI() {
         super("Find Dog App");
         initializeFields();
         initializeGraphics();
-        addButtons();
     }
 
     // EFFECTS: initialze all required fields to their empty states
     private void initializeFields() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'initializeFields'");
+        foundDogs = new ListPersonFound();
+        lostDogs = new ListPersonLost();
+        currentDog = new CurrentDog(null);
+
+        jsonWriter = new JsonWriter(JSON_STORE);
+        jsonReader = new JsonReader(JSON_STORE);
     }
 
     // EFFECTS: sets up the layout of the App
     private void initializeGraphics() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'initializeGraphics'");
+        setLayout(new BorderLayout());
+        setSize(WIDTH, HEIGHT);
+        addButtons();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        pack();
+        setVisible(true);
     }
 
+    // modelled after AlarmControllerUI
+    // Github link: https://github.students.cs.ubc.ca/CPSC210/AlarmSystem
     // EFFECTS: adds buttons for loading/saving, running lost/found version and functionality
     private void addButtons() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addButtons'");
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new GridLayout(2, 2));
+
+        buttonPanel.add(new JButton(new LostDogAction()));
+        buttonPanel.add(new JButton(new FoundDogAction()));
+        buttonPanel.add(new JButton(new SaveAction()));
+        buttonPanel.add(new JButton(new LoadAction()));
+
+        add(buttonPanel, BorderLayout.CENTER);
+
+    }
+
+    private class LostDogAction extends AbstractAction {
+        LostDogAction() {
+            super("Lost Dog Mode");
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            setVisible(false);
+            new LostDogUI(currentDog, foundDogs, lostDogs, FindDogAppUI.this);
+        }
+    }
+
+    private class FoundDogAction extends AbstractAction {
+        FoundDogAction() {
+            super("Found Dog Mode");
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            setVisible(false);
+            new FoundDogUI(currentDog, foundDogs, lostDogs, FindDogAppUI.this);
+        }
+    }
+
+    private class SaveAction extends AbstractAction {
+        SaveAction() {
+            super("Save Data");
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            saveData();
+        }
+    }
+
+    private class LoadAction extends AbstractAction {
+        LoadAction() {
+            super("Load Data");
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            loadData();
+        }
     }
 
     // EFFECTS: saves all current DogApp data to file
     private void saveData() {
-
+        try {
+            jsonWriter.open();
+            jsonWriter.write(currentDog.getDog(), lostDogs, foundDogs);
+            jsonWriter.close();
+            JOptionPane.showMessageDialog(this, "Data saved to " + JSON_STORE);
+        } catch (FileNotFoundException e) {
+            JOptionPane.showMessageDialog(this, "Unable to write to file: " + JSON_STORE);
+        }
     }
 
     // MODIFIES: this
     // EFFECTS: loads DogApp data from file
     private void loadData() {
-
+        try {
+            Object[] data = jsonReader.read();
+            currentDog.setDog((Dog) data[DOG_INDEX]);
+            lostDogs = (ListPersonLost) data[LOST_INDEX];
+            foundDogs = (ListPersonFound) data[FOUND_INDEX];
+            JOptionPane.showMessageDialog(this, "Data loaded from " + JSON_STORE);
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(this, "Unable to read from file: " + JSON_STORE);
+        }
     }
 
     public static void main(String[] args) {
